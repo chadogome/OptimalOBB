35bbafe217dab4e1b7251ce7a1a7dab3810eef4a
Samuel Melchior <samuel.melchior@epfl.ch>
2018-10-01 15:40:35 +0200

    Clean external functions but for findRow

--- src/ORourke/computeR_n.m
+++ src/ORourke/computeR_n.m
@@ -8,13 +8,12 @@ function R_n = computeR_n(theta1,phi,R_x,normal2)
 % MELCHIOR Samuel - UCL, 2008-09.
 % 
 
-n1 = [cos(theta1) sin(theta1) 0];
-n2 = [-sin(theta1)*sin(phi) cos(theta1)*sin(phi) -cos(theta1)*cos(phi)]/sqrt(cos(theta1)^2+sin(theta1)^2*sin(phi)^2);
-sigma = sign(n2*R_x*normal2');
-if sigma == 0
-    sigma = 1;
+ct = cos(theta1); st = sin(theta1);
+cp = cos(phi); sp = sin(phi);
+n1_R_x = [ct, st, 0]*R_x;
+n2_R_x = ([-st*sp, ct*sp, -ct*cp]*R_x)/sqrt(ct*ct + st*st*sp*sp);
+if n2_R_x*normal2' < 0,
+    n2_R_x = -n2_R_x;
 end
-n2 = n2*sigma;
-%n3 = cross(n1,n2); %[n2(3)*sin(theta1) -n2(3)*cos(theta1) -n2(1)/sin(theta1)];   % !!! it bugged for theta1 = 0
-n3 = [n1(2)*n2(3) -n1(1)*n2(3) n1(1)*n2(2)-n1(2)*n2(1)];
-R_n = [n1;n2;n3]*R_x;
+n3_R_x = [n1_R_x(2)*n2_R_x(3)-n1_R_x(3)*n2_R_x(2) n1_R_x(3)*n2_R_x(1)-n1_R_x(1)*n2_R_x(3) n1_R_x(1)*n2_R_x(2)-n1_R_x(2)*n2_R_x(1)];
+R_n = [n1_R_x;n2_R_x;n3_R_x];
